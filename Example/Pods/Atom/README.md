c849cf33d22c10e8a1d1f1b0963e7439c40dc255
Serhiy Vysotskiy <vysotskiyserhiy@gmail.com>
2018-01-22 00:18:47 +0200

    update

--- Example/Pods/Atom/README.md
+++
@@ -1,3 +0,0 @@
-# Atom
-
-Create Atomic (thread safe) values in swift. Improved version of objc.io Atomic
--- Example/Pods/Target Support Files/ViewExtensions/ViewExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ViewExtensions : NSObject
-@end
-@implementation PodsDummy_ViewExtensions
-@end
